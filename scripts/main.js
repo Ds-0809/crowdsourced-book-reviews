fc926cffde9060fb5ba5b8ec5d5e4ea64a5d8a47
Ds-0809 <diyashetty080907@gmail.com>
2025-05-09 10:00:23 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,13 @@ async function loadReviews() {
     });
 }
 loadReviews();
+async function loadReviews() {
+    const response = await fetch('https://api.github.com/repos/[your-username]/book-reviews/contents/reviews');
+    const files = await response.json();
+    
+    files.forEach(async file => {
+        const reviewData = await fetch(file.download_url).then(res => res.json());
+        console.log(reviewData); // Display this in HTML
+    });
+}
+loadReviews();
