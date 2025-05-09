2842d31b71b2990d30c1babf1328aab6a7ee7630
Ds-0809 <diyashetty080907@gmail.com>
2025-05-09 09:35:53 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+async function loadBooks() {
+    const response = await fetch('books/example-book.json');
+    const book = await response.json();
+    
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = `
+        <div class="book">
+            <h2>${book.title}</h2>
+            <p>By ${book.author}</p>
+            <img src="${book.cover_image}" width="100">
+            <p>${book.description}</p>
+        </div>
+    `;
+}
+loadBooks();
 async function loadReviews() {
     const response = await fetch('https://api.github.com/repos/[your-username]/book-reviews/contents/reviews');
     const files = await response.json();
